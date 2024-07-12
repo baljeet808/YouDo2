d084cb7ae1206b563f0468afdc917e39c28c265d
Baljeet singh <bsingh@immediac.com>
2024-07-11 21:52:52 -0300

    added firebase auth dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -45,6 +45,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.firebase.auth)
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
