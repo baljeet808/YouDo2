178120e3f0710697b3ba8a2ca706782c714901a7
Baljeet singh <bsingh@immediac.com>
2024-07-13 13:35:01 -0300

    added koin component tag to application

--- composeApp/src/androidMain/kotlin/com/baljeet/youdo2/MyApplication.kt
+++ composeApp/src/androidMain/kotlin/com/baljeet/youdo2/MyApplication.kt
@@ -3,8 +3,9 @@ package com.baljeet.youdo2
 import android.app.Application
 import di.initKoin
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.component.KoinComponent
 
-class MyApplication : Application() {
+class MyApplication : Application(), KoinComponent {
     override fun onCreate() {
         super.onCreate()
         initKoin{
