832384b942c44dd2332fa51b3074f499dbd91968
bsingh <bsingh@immediac.com>
2024-08-10 11:07:50 -0300

    Fixed the sign up refresh state issue - warning removed

--- composeApp/src/commonMain/kotlin/presentation/signup/SignupScreenEvents.kt
+++ composeApp/src/commonMain/kotlin/presentation/signup/SignupScreenEvents.kt
@@ -1,7 +1,5 @@
 package presentation.signup
 
-import presentation.login.LoginScreenEvents
-
 sealed class SignupScreenEvents {
     data class OnAttemptToSignup(val email : String, val password : String) : SignupScreenEvents()
     data class OnPasswordChange(val password : String) : SignupScreenEvents()
