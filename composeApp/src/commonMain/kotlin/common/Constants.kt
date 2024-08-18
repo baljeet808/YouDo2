04c22a9dbb8edbb374344b166cf63d07cf08ff9f
bsingh <bsingh@immediac.com>
2024-08-17 23:52:44 -0300

    added few more aliens

--- composeApp/src/commonMain/kotlin/common/Constants.kt
+++ composeApp/src/commonMain/kotlin/common/Constants.kt
@@ -73,13 +73,14 @@ fun getOnboardingPagerContentList () : List<OnBoardPagerContent>{
 
 val avatars = arrayListOf(
     "https://firebasestorage.googleapis.com/v0/b/youdo2.appspot.com/o/Alien1.png?alt=media&token=1746e23a-5c21-40f9-b3da-d5b1e1d6e777",
-    "https://firebasestorage.googleapis.com/v0/b/youdotoo-81372.appspot.com/o/20.png?alt=media&token=fa1489d4-8951-4ef6-8f96-862938aedb62",
-    "https://firebasestorage.googleapis.com/v0/b/youdotoo-81372.appspot.com/o/1.png?alt=media&token=b15d14e9-722d-410d-b9b4-23682b5773f3",
-    "https://firebasestorage.googleapis.com/v0/b/youdotoo-81372.appspot.com/o/2.png?alt=media&token=68e95bc9-9553-4027-90d9-af688b9fd0f4",
-    "https://firebasestorage.googleapis.com/v0/b/youdotoo-81372.appspot.com/o/16.png?alt=media&token=160bd284-4b6a-488c-b66b-b421ebde9c21",
-    "https://firebasestorage.googleapis.com/v0/b/youdotoo-81372.appspot.com/o/4.png?alt=media&token=3c06a69a-7f00-4238-b6cf-3296f3532576",
-    "https://firebasestorage.googleapis.com/v0/b/youdotoo-81372.appspot.com/o/6.png?alt=media&token=3fefb778-d6a4-4c05-9d0b-ed8dde6091e5",
-    "https://firebasestorage.googleapis.com/v0/b/youdotoo-81372.appspot.com/o/9.png?alt=media&token=dd24b271-e068-43a6-b0f4-c3142873575c"
+    "https://firebasestorage.googleapis.com/v0/b/youdo2.appspot.com/o/Kukku.png?alt=media&token=e6294874-1044-47cf-ba1c-fbb6cd88802e",
+    "https://firebasestorage.googleapis.com/v0/b/youdo2.appspot.com/o/Alien8.png?alt=media&token=dca1009d-19db-4bee-8877-d70173f5ad2f",
+    "https://firebasestorage.googleapis.com/v0/b/youdo2.appspot.com/o/Alien7.png?alt=media&token=02f052c2-54c8-4f8b-9cf8-b5250c099359",
+    "https://firebasestorage.googleapis.com/v0/b/youdo2.appspot.com/o/Alien6.png?alt=media&token=e7741e0a-4f78-4da5-bc46-af22115d732c",
+    "https://firebasestorage.googleapis.com/v0/b/youdo2.appspot.com/o/Alien5.png?alt=media&token=73067112-601c-43f7-bdea-eff17e7fa788",
+    "https://firebasestorage.googleapis.com/v0/b/youdo2.appspot.com/o/Alien4.png?alt=media&token=2ff3741a-8463-41ba-bb08-32d3d7cbc48b",
+    "https://firebasestorage.googleapis.com/v0/b/youdo2.appspot.com/o/Alien3.png?alt=media&token=e83444d5-2e5b-46e7-b60a-b091b8259b55",
+    "https://firebasestorage.googleapis.com/v0/b/youdo2.appspot.com/o/Alien2.png?alt=media&token=9315b56f-0c79-449e-843f-ca6d45052f33",
 )
 
 val loginHeadings = arrayOf(
